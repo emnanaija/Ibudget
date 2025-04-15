4e86c67ec2b532589b3cc55a59c4839caaef8fe7
emna-naija-pro <emnanaija@gmail.com>
2025-04-15 21:07:37 +0100

    front changes in depenses

--- i-budget/src/app/FrontOffice/pages/page-des-depenses/page-des-depenses.component.ts
+++ i-budget/src/app/FrontOffice/pages/page-des-depenses/page-des-depenses.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DepensesComponent } from '../../components/depenses/depenses.component'; // adapte le chemin selon ton projet
 import { SidebarComponent } from '../../sidebar/sidebar.component';
-
+import { HeaderComponent } from '../../header/header.component';
 @Component({
   selector: 'app-page-desDepenses',
   standalone: true,
-  imports: [CommonModule, DepensesComponent,
+  imports: [CommonModule, DepensesComponent,SidebarComponent,HeaderComponent
       ],
   templateUrl: './page-des-Depenses.component.html',
   styleUrls: ['./page-des-Depenses.component.css']
