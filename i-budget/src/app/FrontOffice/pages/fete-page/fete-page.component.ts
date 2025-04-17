9d231f20c3369339378d8edc504da62b0f7a3627
emna-naija-pro <emnanaija@gmail.com>
2025-04-17 19:14:08 +0100

    colors for holiday component modified with  degrade

--- i-budget/src/app/FrontOffice/pages/fete-page/fete-page.component.ts
+++ i-budget/src/app/FrontOffice/pages/fete-page/fete-page.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
 import { FeteComponent } from '../../components/fete/fete.component';  // Importer le composant FeteComponent
+
+
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DepensesComponent } from '../../components/depenses/depenses.component'; // adapte le chemin selon ton projet
+import { SidebarComponent } from '../../sidebar/sidebar.component';
+import { HeaderComponent } from '../../header/header.component';
+import {AjoutDepenseComponent } from '../../components/ajout-depense/ajout-depense.component'; // adapte le chemin selon ton projet
 
 @Component({
   selector: 'app-fete-page',
-  standalone: true,  // Déclarer ce composant comme standalone
-  imports: [FeteComponent],  // Importer le composant FeteComponent ici
-  template: `
-    <app-fete></app-fete>  <!-- Utilisation du composant FeteComponent -->
-  `
+  standalone: true,
+  imports: [CommonModule,SidebarComponent,HeaderComponent,FeteComponent
+      ],
+  templateUrl: './fete-page.component.html',
+  styleUrls: ['./fete-page.component.css']
 })
 export class FetePageComponent {
+  isSidebarCollapsed = false; // Propriété définie, initialisée à false par défaut
 
+  toggleSidebar() {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed; // Inverse la valeur de isSidebarCollapsed
+  }
 }
